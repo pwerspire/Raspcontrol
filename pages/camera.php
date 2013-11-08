7128d6a4388e38fb9c8c7abf18d176588b837e67
josejamilena <josejamilena@gmail.com>
2013-11-08 15:53:35 +0100

    error en commit anterior

--- pages/camera.php
+++ pages/camera.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<iframe src="<?php echo("http://".$_SERVER['HTTP_HOST'].":9090/")?>"  width="100%" height="600">
+<iframe src="<?php echo("http://".$_SERVER['HTTP_HOST'].":9090/javascript_simple.html")?>"  width="100%" height="600">
   
 </iframe>
 
